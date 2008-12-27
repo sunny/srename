9bcad6208ae9eb9c670cdab93d72248b0f09fc83
Sunny Ripert <negatif@gmail.com>
2008-12-27 01:28:37 +0100

    Now supports different extensions! Can now rename srts independantly! No more anonymous rescues! Rejoice!

--- srename.rb
+++ srename.rb
@@ -24,41 +24,52 @@ require 'rubygems'
 require 'highline/import'
 require 'fileutils'
 
-files = Dir.glob('*.avi').sort
-abort "Error: no .avi found!" if files.size == 0
-
-first_title = files.first.gsub(/\.[a-z]+$/, '')
-prefix_guess = first_title.match(/^[^0-9]+[a-z]/i)[0] rescue ""
-prefix = ask("Prefix? ") { |q|
-  q.default = prefix_guess if prefix_guess != ""
-}
-
-season_guess = first_title.match(/[1-9]/)[0].to_i rescue 1
-season = ask("Season? ", Integer) { |q|
-  q.default = season_guess == 0 ? 1 : season_guess
-  q.in = 1..900
-}
+EXTS = %w(avi srt mpeg xvid)
 
 renames = {}
-files.each do |file|
-  number_guess = file.sub(season.to_s, '').match(/[1-9][0-9]*/)[0].to_i rescue 0
-  number = ask("Episode number for \"#{file}\" (0 to skip) ? ", Integer) { |q|
-    q.default = number_guess
-    q.in = 0..900
+prefix = nil
+season = 1
+
+EXTS.each do |ext|
+  files = Dir.glob("*.#{ext}").sort
+  next if files.empty?
+
+  puts "Found #{files.size} #{ext} files"
+
+  first_title = files.first.gsub(/\.[a-z]+$/, '')
+  prefix_match = first_title.match(/^[^0-9]+[a-z]/i)
+  prefix_guess = !prefix_match.nil? && prefix_match[0] != "" ? prefix_match[0] : prefix
+  prefix = ask("Prefix? ") { |q|
+    q.default = prefix_guess if prefix_guess
   }
-  next if number == 0
-  title = "%s-%s%02i" % [prefix, season, number]
-  subf = file.gsub(/avi$/, 'srt')
-  renames["#{title}.avi"] = file
-  renames["#{title}.srt"] = subf if File.file?(subf)
+
+  season_match = first_title.match(/[1-9]/)
+  season_guess = !season_match.nil? && season_match[0] != '0' ? season_match[0].to_i : season 
+  season = ask("Season? ", Integer) { |q|
+    q.default = season_guess
+    q.in = 1..900
+  }
+
+  files.each do |file|
+    number_match = file.sub(season.to_s, '').match(/[1-9][0-9]*/)
+    number_guess = !number_match.nil? ? number_match[0].to_i : 0
+    number = ask("Episode number for \"#{file}\"? (0 to skip) ", Integer) { |q|
+      q.default = number_guess
+      q.in = 0..900
+    }
+    next if number == 0
+    title = "%s-%s%02i" % [prefix, season, number]
+    renames["#{title}.#{ext}"] = file
+  end
+
 end
 
+renames = renames.select { |to, from| to != from }.sort
+abort "No files to rename found!" if renames.empty?
+
 puts "About to rename:"
-renames = renames.to_a.sort
 renames.each { |to, from| puts "#{to} (#{from})" }
 
 exit unless agree "Really rename all files? "
-renames.each { |to, from|
-  FileUtils.mv(from, to) unless from == to
-}
+renames.each { |to, from| FileUtils.mv(from, to) }
 
